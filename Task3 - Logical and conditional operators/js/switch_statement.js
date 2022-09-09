b63b818f1f3416e888ffda99a27a894914ebbe6b
kirilmats <mfkirill7@gmail.com>
2022-09-09 11:29:14 -0500

    Fixed 2 point (swith-case) in Task 3

--- Task3 - Logical and conditional operators/js/switch_statement.js
+++ Task3 - Logical and conditional operators/js/switch_statement.js
@@ -1,25 +1,45 @@
-const checkEqualitySw = () => {
-    let num = prompt('Please enter three-digit number', 0);
-    const notAThreeDigit = num.length > 3 || num.length < 3;
-    const anyEqual = num[0] === num[1] || num[1] === num[2] || num[0] === num[2];
-    const allEqual = num[0] === num[1] && num[1] === num[2];
-    
-    switch (true) {
-        case (notAThreeDigit):
-            alert('The number you enter must be three-digit. Please try again');
-            break;
+const checkNumber = () => {
+    let num = Number(prompt('Please enter a number in the range from 1 to 3', 0));
 
-        case (allEqual):
-            alert('All numbers are equal');
+    switch (num){
+        case 1:
+            console.log('You\'ve entered number 1');
             break;
-
-        case (anyEqual):
-            alert("Some of the numbers are equal");
+        case 2:
+            console.log('You\'ve entered number 2');
             break;
-
+        case 3:
+            console.log('You\'ve entered number 3');
+            break;
         default:
-            alert('None of the numbers are equal');
+            console.log('The number you\'ve entered is not in the range from 1 to 3 or is not a number');
     }
 }
+checkNumber();
 
-checkEqualitySw();
+
+// const checkEqualitySw = () => {
+//     let num = prompt('Please enter three-digit number', 0);
+//     const notAThreeDigit = num.length > 3 || num.length < 3;
+//     const anyEqual = num[0] === num[1] || num[1] === num[2] || num[0] === num[2];
+//     const allEqual = num[0] === num[1] && num[1] === num[2];
+    
+//     switch (true) {
+//         case (notAThreeDigit):
+//             alert('The number you enter must be three-digit. Please try again');
+//             break;
+
+//         case (allEqual):
+//             alert('All numbers are equal');
+//             break;
+
+//         case (anyEqual):
+//             alert("Some of the numbers are equal");
+//             break;
+
+//         default:
+//             alert('None of the numbers are equal');
+//     }
+// }
+
+// checkEqualitySw();
