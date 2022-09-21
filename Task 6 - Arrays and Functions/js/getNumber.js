fcb2188c2b1b194e2e0d5a512ff4b7c0b983544c
kirilmats <mfkirill7@gmail.com>
2022-09-21 09:39:45 -0500

    Fixed 1st point of Task 7: added isNaN check to catch if user enters text instead of number, also replaces alerts with console logs due to the task instruction

--- Task 6 - Arrays and Functions/js/getNumber.js
+++ Task 6 - Arrays and Functions/js/getNumber.js
@@ -2,30 +2,31 @@ const getNumber = () => {
     // let i = 0;
     // while(i < 10) {
     //     i++;
-    //     let value = +prompt('Please enter a number greater than 100', 0);
-    //     if(value > 100) {
-    //         alert(`You've entered a number: ${value}`);
+    //     let value = prompt('Please enter a number greater than 100', 0);
+    //     if(isNaN(+value) || +value > 100) {
+    //         console.log(`You've entered this: ${value}`);
     //         break;
     //     } else {
     //     if(i === 10) {
-    //         alert('You have reached the limit of attempts :)');
+    //         console.log(`You have reached the limit of attempts. The last value you've entered is: ${value}`);
     //         break;
     //     }
-    //     alert('The number must be greater than 100. Please try again :)');
-    // }
+    //     alert(`You've entered this: ${value}. The number must be greater than 100. Please try again. (Attempts remaining: ${10 - i}))`);
+    //     }
     // }
 
     for(let i = 0; i < 10; i++) {
-        let value = +prompt('Please enter a number greater than 100', 0);
-        if(value > 100) {
-            alert(`You've entered a number: ${value}`);
+        let value = prompt('Please enter a number greater than 100', 0);
+
+        if(isNaN(+value) || +value > 100) {
+            console.log(`You've entered this: ${value}`);
             break;
         } else {
             if(i === 9) {
-                alert('You have reached the limit of attempts :)');
+                console.log(`You have reached the limit of attempts. The last value you've entered is: ${value}`);
                 break;
             }
-            alert('The number must be greater than 100. Please try again :)');
+            alert(`You've entered this: ${value}. The number must be greater than 100. Please try again. (Attempts remaining: ${9 - i}))`);
         }
     }
 }
