9f121c32acf1a8141b5dc588081e6f6ba63360af
kirilmats <mfkirill7@gmail.com>
2022-10-25 00:01:06 +0300

    added disable/enable buttons

--- Task 15 - Slider with markers; Clock/Clock/js/class.js
+++ Task 15 - Slider with markers; Clock/Clock/js/class.js
@@ -8,35 +8,51 @@ class Clock {
         this._stop = _stop;
         this._resume = _resume;
     }
-
     init() {
         this._displayTime();
         this._resumeClock();
         this._startClock();
         this._stopClock(this._time);
+        this._disableButton(this._resume);
     }
     _displayTime() {
-        const time = new Date();
-        const hours = time.getHours();
-        const minutes = time.getMinutes();
-        const seconds = time.getSeconds();
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        const minutes = currentTime.getMinutes();
+        const seconds = currentTime.getSeconds();
         this._hours.innerHTML = hours < 10 ? `0${hours}` : hours;
         this._minutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
         this._seconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
     }
+    _disableButton(button) {
+        if(button === this._resume) {
+            this._resume.setAttribute('disabled', '');
+        } else if(button === this._stop) {
+            this._stop.setAttribute('disabled', '');
+        }
+    }
+    _enableButton(button) {
+        if(button === this._resume) {
+            this._resume.removeAttribute('disabled');
+        } else if(button === this._stop) {
+            this._stop.removeAttribute('disabled');
+        }
+    }
     _resumeClock() {
         this._resume.addEventListener('click', e => {
             this._startClock();
+            this._disableButton(this._resume);
+            this._enableButton(this._stop);
         })
     }
     _startClock() {
-        this._time = setInterval(() => {
-            this._displayTime();
-        }, 1000)
+        this._time = setInterval(() => this._displayTime(), 1000)
     }
     _stopClock() {
         this._stop.addEventListener('click', e => {
             clearInterval(this._time);
+            this._disableButton(this._stop);
+            this._enableButton(this._resume);
         })
     }
 }
--- Task 15 - Slider with markers; Clock/Clock/js/constructor.js
+++ Task 15 - Slider with markers; Clock/Clock/js/constructor.js
@@ -1,34 +1,51 @@
 'use strict';
 function Clock({_hours, _minutes, _seconds, _stop, _resume}) {
     this._time = undefined;
-    this.init = function () {
+    this.init = function() {
         this._displayTime();
         this._resumeClock();
         this._startClock();
-        this._stopClock(this._time);
+        this._stopClock();
+        this._disableButton(_resume);
     }
-    this._displayTime = function () {
-        const time = new Date();
-        const hours = time.getHours();
-        const minutes = time.getMinutes();
-        const seconds = time.getSeconds();
+    this._displayTime = function() {
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        const minutes = currentTime.getMinutes();
+        const seconds = currentTime.getSeconds();
         _hours.innerHTML = hours < 10 ? `0${hours}` : hours;
         _minutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
         _seconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
     }
-    this._resumeClock = function () {
+    this._disableButton = function(button) {
+        if(button === _resume) {
+            _resume.setAttribute('disabled', '');
+        } else if(button === _stop) {
+            _stop.setAttribute('disabled', '');
+        }
+    }
+    this._enableButton = function(button) {
+        if(button === _resume) {
+            _resume.removeAttribute('disabled');
+        } else if(button === _stop) {
+            _stop.removeAttribute('disabled');
+        }
+    }
+    this._resumeClock = function() {
         _resume.addEventListener('click', e => {
             this._startClock();
+            this._disableButton(_resume);
+            this._enableButton(_stop);
         })
     }
-    this._startClock = function () {
-        this._time = setInterval(() => {
-            this._displayTime();
-        }, 1000)
+    this._startClock = function() {
+        this._time = setInterval(() => this._displayTime(), 1000)
     }
-    this._stopClock = function () {
+    this._stopClock = function() {
         _stop.addEventListener('click', e => {
             clearInterval(this._time);
+            this._disableButton(_stop);
+            this._enableButton(_resume);
         })
     }
 }
