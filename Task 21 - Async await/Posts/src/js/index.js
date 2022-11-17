89a18e03583df3321f0af441971fbfe61f26a742
kirilmats <mfkirill7@gmail.com>
2022-11-17 13:37:15 -0600

    Task 21 - Async await

---
+++ Task 21 - Async await/Posts/src/js/index.js
@@ -0,0 +1,85 @@
+'use strict';
+const API_JSON_URL = 'https://jsonplaceholder.typicode.com/posts/';
+document.addEventListener('DOMContentLoaded', function () {
+    class LoadPost {
+        constructor({_postBody, _postComments, _postInput, _postForm, _commentsCount}) {
+            this._postBody = _postBody;
+            this._postComments = _postComments;
+            this._postInput = _postInput;
+            this._postForm = _postForm;
+            this._commentsCount = _commentsCount;
+        }
+        init() {
+            this._getPostHandler();
+        }
+        _getPostId() {
+            return this._postInput.value;
+        }
+        async _getPostData() {
+            const url = API_JSON_URL + this._getPostId();
+            const res = await fetch(url, {method: "GET"});
+            const data = await res.json();
+            if(data.id) {
+                this._addPost(data);
+                this._getPostComments(url);
+            }
+        }
+        _getPostHandler() {
+            this._postForm.addEventListener('submit', (e) => {
+                e.preventDefault();
+                this._getPostData();
+            })
+        }
+        _addPost(postData) {
+            this._postBody.innerHTML = postData.body;
+        }
+        async _getPostComments(url) {
+            const urlComments = url + '/comments';
+            const res = await fetch(urlComments, {method: "GET"});
+            const data = await res.json();
+
+            this._clearPostComments();
+            data.forEach(comment => {
+                this._addComment(comment);
+            });
+            this._setCommentsCount(data.length);
+        }
+        _clearPostComments() {
+            this._postComments.innerHTML = '';
+        }
+        _setCommentsCount(count) {
+            this._commentsCount.innerHTML = count;
+        }
+        _addComment(commentData) {
+            const comment = this._createComment(commentData);
+            this._postComments.insertAdjacentHTML('beforeend', comment);
+        }
+        _createComment(commentData) {
+            const name = commentData.email.split('@')[0];
+            return `
+                <div class="comment">
+                            <div class="user-info">
+                                <a class="user-link" href="#">
+                                    <img class="user-photo" src="https://via.placeholder.com/100/1d9bf0/1d9bf0?Text=Down.com" alt="">
+                                    <div class="user-names">
+                                        <p class="user-name">${name}</p>
+                                        <p class="user-at-name">@${name}</p>
+                                    </div>
+                                </a>
+                                <div class="user-options">&bull;&bull;&bull;</div>
+                            </div>
+                            <div class="comment__body">${commentData.body}</div>
+                            <div class="divider"></div>
+                        </div>
+            `
+        }
+    }
+    const loadPost = new LoadPost({
+        _postBody: document.querySelector('.js--post__body'),
+        _postComments: document.querySelector('.js--comments'),
+        _postInput: document.querySelector('.post__input'),
+        _postForm: document.querySelector('.js--post__form'),
+        _commentsCount: document.querySelector('.js--comments-count')
+    });
+    loadPost.init();
+});
